7c8d5b88a2d01bc69c69567559a9e2c48a072ac7
Meagan Utsey <meagan.utsey@gmail.com>
2019-10-06 22:20:24 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,15 @@
-# Make your shoe class here!
+#shoe.rb 
+
+class shoe 
+attr_accessor :brand, :page_count, :genre
+  attr_reader :title
+ 
+  def initialize(title)
+    @title = title
+  end
+ 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+ 
+end
